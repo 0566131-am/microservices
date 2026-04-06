f07430b41a9c755475da86ffe0437c7717a5c340
0566131-am <your-github-email@gmail.com>
2026-04-06 22:26:25 +0530

    Initial commit - Spring Boot microservices project

---
+++ microservices-eureka/src/main/java/com/example/microservices_eureka/MicroservicesEurekaApplication.java
@@ -0,0 +1,14 @@
+package com.example.microservices_eureka;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class MicroservicesEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicesEurekaApplication.class, args);
+	}
+
+}
